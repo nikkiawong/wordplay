b2e160c0bf3b6dae472e7853f53eff2b67535ded
Krystal Foster and Nikki Wong <nikki@>
2018-08-13 13:59:23 -0700

    add split, reverse and join methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    function splitString(stringToSplit, separator) {
-      var arrayOfStrings = stringToSplit.split(separator);
-    }
-    var inputString = $("input#sentence").val();
-    var space = ' ';
+    // function splitString(stringToSplit, ' ') {
+    //   var arrayOfStrings = stringToSplit.split(separator);
+    // }
+    // var inputString = $("input#sentence").val();
+    //
+    //
+    // console.log(splitString(inputString, space));
 
-    splitString(inputString, space);
-  });
+    var str = $("input#sentence").val();
+
+
+      if (str.length >= 3) {
+      var strSplits = str.split(' ').reverse().join(' ');
+      console.log(strSplits);
+    // });
+
+    // var strSplits = str.split(' ');
+
+};
+
+  // if (str.length >= 3) {
+  //   str.map(function(str) {
+  //     var joinedStr = str.re
+  //   })
+  //  }
+});
 });
